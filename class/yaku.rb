6199f106bb927451a5bcca2a7ed20e9bd0e9425e
giginet <giginet.net@gmail.com>
2010-05-05 17:03:37 +0900

    addしてなかった

---
+++ class/yaku.rb
@@ -0,0 +1,23 @@
+#役クラス
+class Yaku
+  def initialize(name,fan)
+      @name = name
+      @fan = fan
+      @score = get_score
+      #役満なら１、ダブル役満なら２
+      @yakuman = (@fan/13).floor
+  end
+  def get_score
+      #飜数から点数を返す
+  end
+  def get_name
+      return @name
+  end
+  def get_fan
+    return @fan
+  end
+  def yakuman?
+    return @yaku>0
+  end
+  attr_reader :score,:fan
+end
